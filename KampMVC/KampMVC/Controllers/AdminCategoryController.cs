8a5092621a3b0d91408aec0cd027c770dde7827c
Furkan Kapukaya <furkankapukayaa@gmail.com>
2021-05-07 12:06:13 +0300

    ders 33-36 entegre
    Temanın Yüklenmesi
    Temanın Düzenlenmesi
    Sidebar'da Son Düzenlemeler
    Sidebar İle Beraber Kategori İşlemleri

---
+++ KampMVC/KampMVC/Controllers/AdminCategoryController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules_FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KampMVC.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryDAL());
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryvalidator = new CategoryValidator();
+            ValidationResult results = categoryvalidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAddBL(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
