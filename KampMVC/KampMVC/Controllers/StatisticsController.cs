acd401c47316b60e6a0c63017af49aec0c5133fc
Furkan Kapukaya <furkankapukayaa@gmail.com>
2021-05-11 19:26:01 +0300

    Revert "Revert "sertifika görevi""
    This reverts commit f5a5839dd5e33dd742461895e1f4c532a156c164.

---
+++ KampMVC/KampMVC/Controllers/StatisticsController.cs
@@ -0,0 +1,29 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KampMVC.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var result = context.Categories.Count();
+            var result2 = context.Headings.Count(x => x.Category.CategoryID == 1024);
+            var result3 = context.Writers.Count(x => x.WriterName.Contains("a"));
+            var result4 = context.Headings.Max(x => x.Category.CategoryName);
+            var result5 = context.Categories.Count(x => x.CategoryStatus == true);
+            var result6 = context.Categories.Count(x => x.CategoryStatus == false);
+            ViewBag.CategoryCount = result;
+            ViewBag.Heading = result2;
+            ViewBag.Writer = result3;
+            ViewBag.HeadingMax = result4;
+            ViewBag.Status = (result5 - result6);
+            return View();
+        }
+    }
+}
