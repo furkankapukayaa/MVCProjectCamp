a3f97ffc9463f03bcf933979c942af1fd58ce8d8
Furkan Kapukaya <furkankapukayaa@gmail.com>
2021-05-14 22:05:30 +0300

    Writer Validator Update

--- KampMVC/BusinessLayer/ValidationRules-FluentValidation/WriterValidator.cs
+++ KampMVC/BusinessLayer/ValidationRules-FluentValidation/WriterValidator.cs
@@ -17,6 +17,16 @@ namespace BusinessLayer.ValidationRules_FluentValidation
             RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Yazar Hakkındayı Boş Geçemezsiniz.");
             RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("En Az 3 Karakter Girişi Yapın.");
             RuleFor(x => x.WriterSurname).MaximumLength(20).WithMessage("En Fazla 20 Karakter Girişi Yapın.");
+            RuleFor(x => x.WriterAbout).Must(MustBeA).WithMessage("Hakkında Kısmında Mutlaka A Harfi Olmalıdır.");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazar Maili Boş Geçemezsiniz");
+            RuleFor(x => x.WriterImage).NotEmpty().WithMessage("Yazar Fotoğrafı Boş Geçemezsiniz");
+            RuleFor(x => x.WriterMail).EmailAddress().WithMessage("Yazar Maili Mail Adresi Türünde Olmalıdır.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Yazar Şifreyi Boş Geçemezsiniz");
+            RuleFor(x => x.WriterPassword).MinimumLength(3).WithMessage("En Az 3 Karakter Girişi Yapın.");
+        }
+        private bool MustBeA(string arg)
+        {
+            return arg.Contains("A");
         }
     }
 }
